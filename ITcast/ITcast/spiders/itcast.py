dab5128de2d2eb6ba8415fb4228f741b4b80d702
zcdll <qinsword521@gmail.com>
2018-01-17 14:52:43 +0800

    init

---
+++ ITcast/ITcast/spiders/itcast.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class ItcastSpider(scrapy.Spider):
+    name = 'itcast'
+    allowed_domains = ['itcast.cn']
+    start_urls = ['http://www.itcast.cn/channel/teacher.shtml']
+
+    def parse(self, response):
+        node_list = response.xpath("//div[@class='li_txt']")
+        for node in node_list:
+            name = node.xpath("./h3/text()").extract()
+            title = node.xpath("./h4/text()").extract()
+            info = node.xpath("./p/text()").extract()
+
+            print(name[0], title[0], info[0])
