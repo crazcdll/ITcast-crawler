c3469f7f2b1d48b64082184fe8343d80ccd1bbe4
zcdll <qinsword521@gmail.com>
2018-01-18 18:31:32 +0800

    添加管道

--- ITcast/spiders/itcast.py
+++ ITcast/spiders/itcast.py
@@ -3,13 +3,14 @@
 import scrapy
 from ITcast.items import ItcastItem
 
+
 class ItcastSpider(scrapy.Spider):
     name = 'itcast'
     allowed_domains = ['itcast.cn']
     start_urls = ['http://www.itcast.cn/channel/teacher.shtml']
 
     def parse(self, response):
-        items = []
+        # items = []
         node_list = response.xpath("//div[@class='li_txt']")
         for node in node_list:
             item = ItcastItem()
@@ -22,6 +23,8 @@ class ItcastSpider(scrapy.Spider):
             item['title'] = title[0]
             item['info'] = info[0]
 
-            items.append(item)
+            # 返回提取到的每个 item 数据，给管道文件处理，同时返回回来，继续执行后面的代码（循环）
+            yield item
+            # items.append(item)
         # print(items)
-        return items
+        # return items
